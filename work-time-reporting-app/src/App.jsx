522e8a279ba753684e2a20fb1de82c9752a4459f
Karolina Franczuk <karo.franczuk@gmail.com>
2021-06-29 20:05:31 +0200

    Changes in app sructure

--- work-time-reporting-app/src/App.jsx
+++ work-time-reporting-app/src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/Header';
 import WeekTable from './components/WeekTable';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Content from './components/Content';
 
 const App = () => (
     <>
         <Navbar />
-        <WeekTable />
+        <Content />
+        <Footer />
     </>
 )
 
---
+++ work-time-reporting-app/src/components/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = () => {
+return(
+    <h1>content</h1>
+)
+}
+
+export default Content;
